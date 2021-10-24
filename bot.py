45070777edeb9950eba70d58f89c2c00e1dc2ed0
DakshSingh2998 <31823365+DakshSingh2998@users.noreply.github.com>
2021-10-24 12:12:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -313,6 +313,8 @@ def api_id():
     idd=idd.replace("%", " ")
     return jsonify(str(response(idd)))
 
-app.run()
+import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
 
